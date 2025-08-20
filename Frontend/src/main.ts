ff30d186e62f26309ed07aad15848282accaa3ba
ichichchch <ichistudio@163.com>
2025-08-20 23:57:56 +0800

    FrontendFeat：更新下布局

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,6 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ArcoVue from '@arco-design/web-vue';
-import '@arco-design/web-vue/dist/arco.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).use(ArcoVue).mount('#app')
+createApp(App).use().mount('#app');
